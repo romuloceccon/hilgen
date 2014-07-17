854d00c70e3d77fd92fe64868b83b334094c28c0
Romulo A. Ceccon <romuloceccon@gmail.com>
2014-07-17 14:40:38 -0300

    Authentication code refactored into separate class

---
+++ src/com/romuloceccon/hilgen/Authentication.java
@@ -0,0 +1,205 @@
+package com.romuloceccon.hilgen;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import com.googlecode.flickrjandroid.Flickr;
+import com.googlecode.flickrjandroid.FlickrException;
+import com.googlecode.flickrjandroid.auth.Permission;
+import com.googlecode.flickrjandroid.oauth.OAuth;
+import com.googlecode.flickrjandroid.oauth.OAuthInterface;
+import com.googlecode.flickrjandroid.oauth.OAuthToken;
+import com.googlecode.flickrjandroid.people.User;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public class Authentication
+{
+    private static final String TAG = "AUTH";
+    
+    private static final String OAUTH_PREFS = "oauth";
+    
+    private static final String KEY_STATE = "state";
+    private static final String KEY_USER_NAME = "user_name";
+    private static final String KEY_USER_ID= "user_id";
+    private static final String KEY_TOKEN = "token";
+    private static final String KEY_TOKEN_SECRET = "token_secret";
+    
+    public static final int UNAUTHORIZED = 0;
+    public static final int REQUESTING_AUTHORIZATION = 1;
+    public static final int AUTHORIZED = 2;
+    
+    private static Authentication instance = null;
+    
+    private OAuthInterface oAuthApi;
+    
+    private SharedPreferences prefs;
+    
+    private int state = 0;
+    private String oAuthTokenSecret = null;
+    private OAuth oAuth = null;
+    
+    public static synchronized Authentication getInstance(Context ctx, Flickr flickr)
+    {
+        if (instance == null)
+            instance = new Authentication(ctx, flickr);
+        return instance;
+    }
+    
+    private Authentication(Context ctx, Flickr flickr)
+    {
+        oAuthApi = flickr.getOAuthInterface();
+        prefs = ctx.getSharedPreferences(OAUTH_PREFS, Context.MODE_PRIVATE);
+        
+        loadState();
+    }
+    
+    public int getState()
+    {
+        return state;
+    }
+    
+    public String getOAuthTokenSecret()
+    {
+        return oAuthTokenSecret; 
+    }
+    
+    public OAuth getOAuth()
+    {
+        return oAuth;
+    }
+    
+    public String startAuthentication(String scheme)
+    {
+        // We should allow starting the authentication process either in the
+        // UNAUTHORIZED or REQUESTING_AUTHORIZATION states; otherwise we risk
+        // getting stuck in the REQUESTING_AUTHORIZATION state in case of
+        // a failure
+        if (state == AUTHORIZED)
+            return null;
+        
+        String callbackUrl = scheme + "://callback";
+        
+        OAuthToken oAuthToken;
+        String result;
+        
+        try
+        {
+            oAuthToken = oAuthApi.getRequestToken(callbackUrl);
+        }
+        catch (IOException e)
+        {
+            Log.w(TAG, e);
+            return null;
+        }
+        catch (FlickrException e)
+        {
+            Log.w(TAG, e);
+            return null;
+        }
+        
+        try
+        {
+            result = oAuthApi.buildAuthenticationUrl(
+                    Permission.READ, oAuthToken).toString();
+        }
+        catch (MalformedURLException e)
+        {
+            Log.w(TAG, e);
+            return null;
+        }
+        
+        String tokenSecret = oAuthToken.getOauthTokenSecret();
+        
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_STATE, REQUESTING_AUTHORIZATION);
+        editor.putString(KEY_TOKEN_SECRET, tokenSecret);
+        if (!editor.commit())
+            return null;
+        
+        setVariables(REQUESTING_AUTHORIZATION, tokenSecret, null);
+        return result;
+    }
+    
+    public boolean finishAuthentication(String token, String verifier)
+    {
+        if (state != REQUESTING_AUTHORIZATION)
+            return false;
+        
+        OAuth oAuth;
+        
+        try
+        {
+            oAuth = oAuthApi.getAccessToken(token, oAuthTokenSecret, verifier);
+        }
+        catch (IOException e)
+        {
+            Log.w(TAG, e);
+            return false;
+        }
+        catch (FlickrException e)
+        {
+            Log.w(TAG, e);
+            return false;
+        }
+        
+        User user = oAuth.getUser();
+        OAuthToken oAuthToken = oAuth.getToken();
+        
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt(KEY_STATE, AUTHORIZED);
+        editor.putString(KEY_USER_NAME, user.getUsername());
+        editor.putString(KEY_USER_ID, user.getId());
+        editor.putString(KEY_TOKEN, oAuthToken.getOauthToken());
+        editor.putString(KEY_TOKEN_SECRET, oAuthToken.getOauthTokenSecret());
+        if (!editor.commit())
+            return false;
+        
+        setVariables(AUTHORIZED, null, oAuth);
+        return true;
+    }
+    
+    private void loadState()
+    {
+        switch (prefs.getInt(KEY_STATE, 0))
+        {
+        case REQUESTING_AUTHORIZATION:
+            setVariables(REQUESTING_AUTHORIZATION,
+                    prefs.getString(KEY_TOKEN_SECRET, ""), null);
+            break;
+        case AUTHORIZED:
+            setVariables(AUTHORIZED, null, loadOAuth());
+            break;
+        default:
+            setVariables(UNAUTHORIZED, null, null);
+        }
+    }
+    
+    private OAuth loadOAuth()
+    {
+        String userName = prefs.getString(KEY_USER_NAME, "");
+        String userId = prefs.getString(KEY_USER_ID, "");
+        String token = prefs.getString(KEY_TOKEN, "");
+        String tokenSecret = prefs.getString(KEY_TOKEN_SECRET, "");
+        
+        OAuth result = new OAuth();
+        
+        User user = new User();
+        user.setUsername(userName);
+        user.setId(userId);
+        
+        result.setUser(user);
+        result.setToken(new OAuthToken(token, tokenSecret));
+        
+        return result;
+    }
+    
+    private void setVariables(int state, String oAuthTokenSecret, OAuth oAuth)
+    {
+        this.state = state;
+        this.oAuthTokenSecret = oAuthTokenSecret;
+        this.oAuth = oAuth;
+    }
+}
--- src/com/romuloceccon/hilgen/MainActivity.java
+++ src/com/romuloceccon/hilgen/MainActivity.java
@@ -1,21 +1,10 @@
 package com.romuloceccon.hilgen;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-
 import com.googlecode.flickrjandroid.Flickr;
-import com.googlecode.flickrjandroid.FlickrException;
-import com.googlecode.flickrjandroid.auth.Permission;
-import com.googlecode.flickrjandroid.oauth.OAuth;
-import com.googlecode.flickrjandroid.oauth.OAuthInterface;
-import com.googlecode.flickrjandroid.oauth.OAuthToken;
 import com.googlecode.flickrjandroid.people.User;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -26,21 +15,16 @@ public class MainActivity extends Activity
     private static final String TAG = "HILGen";
     private static final String SCHEME = "hilgen";
     
-    private static final String OAUTH_PREFS = "oauth";
-    
-    private static final String KEY_USER_NAME = "user_name";
-    private static final String KEY_USER_ID= "user_id";
-    private static final String KEY_TOKEN = "token";
-    private static final String KEY_TOKEN_SECRET = "token_secret";
-    
     private static Flickr flickrInstance = null;
     
-    private class OauthAuthenticationRequestTask extends AsyncTask<Void, Integer, String>
+    private Authentication authentication;
+    
+    private class OAuthStartAuthenticationTask extends AsyncTask<Void, Integer, String>
     {
         @Override
         protected String doInBackground(Void... params)
         {
-            return requestOauthAuthentication();
+            return authentication.startAuthentication(SCHEME);
         }
         
         @Override
@@ -50,19 +34,18 @@ public class MainActivity extends Activity
         }
     }
     
-    private class OauthAccessTokenRequestTask extends AsyncTask<String, Integer, OAuth>
+    private class OAuthFinishAuthenticationTask extends AsyncTask<String, Integer, Boolean>
     {
-
         @Override
-        protected OAuth doInBackground(String... params)
+        protected Boolean doInBackground(String... params)
         {
-            return requestAccessToken(params[0], params[1], params[2]);
+            return authentication.finishAuthentication(params[0], params[1]);
         }
         
         @Override
-        protected void onPostExecute(OAuth result)
+        protected void onPostExecute(Boolean result)
         {
-            saveOAuth(result);
+            Log.i(TAG, result ? "OAuth success" : "OAuth failure");
         }
     }
     
@@ -72,8 +55,11 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        if (loadOAuth().getToken() == null)
-            new OauthAuthenticationRequestTask().execute();
+        authentication = Authentication.getInstance(getApplicationContext(),
+                getFlickr());
+        
+        if (authentication.getState() == Authentication.UNAUTHORIZED)
+            new OAuthStartAuthenticationTask().execute();
     }
     
     @Override
@@ -90,146 +76,12 @@ public class MainActivity extends Activity
             return;
         }
         
-        OAuth oauth = loadOAuth();
-        User user = oauth.getUser();
-        if (user != null)
+        if (authentication.getState() == Authentication.AUTHORIZED)
+        {
+            User user = authentication.getOAuth().getUser();
             Log.i(TAG, String.format("Current logged in user: %s (%s)",
                     user.getUsername(), user.getId()));
-    }
-    
-    private String requestOauthAuthentication()
-    {
-        String callbackUrl = SCHEME + "://callback";
-        Flickr f = getFlickr();
-        
-        OAuthToken oauthToken;
-        URL oauthUrl;
-        
-        try
-        {
-            oauthToken = f.getOAuthInterface().getRequestToken(callbackUrl);
         }
-        catch (IOException e)
-        {
-            Log.w(TAG, e);
-            return null;
-        }
-        catch (FlickrException e)
-        {
-            Log.w(TAG, e);
-            return null;
-        }
-        
-        saveOAuth(null, null, null, oauthToken.getOauthTokenSecret());
-        
-        try
-        {
-            oauthUrl = f.getOAuthInterface().buildAuthenticationUrl(
-                    Permission.READ, oauthToken);
-        }
-        catch (MalformedURLException e)
-        {
-            Log.w(TAG, e);
-            return null;
-        }
-        
-        return oauthUrl.toString();
-    }
-    
-    private OAuth requestAccessToken(String tokenSecret, String token,
-            String verifier)
-    {
-        Flickr f = getFlickr();
-        OAuthInterface oauthApi = f.getOAuthInterface();
-        
-        try
-        {
-            return oauthApi.getAccessToken(token, tokenSecret, verifier);
-        }
-        catch (IOException e)
-        {
-            Log.w(TAG, e);
-            return null;
-        }
-        catch (FlickrException e)
-        {
-            Log.w(TAG, e);
-            return null;
-        }
-    }
-    
-    private void saveOAuth(String userName, String userId, String token,
-            String tokenSecret)
-    {
-        SharedPreferences.Editor editor = getSharedPreferences(OAUTH_PREFS,
-                Context.MODE_PRIVATE).edit();
-        
-        editor.putString(KEY_USER_NAME, userName);
-        editor.putString(KEY_USER_ID, userId);
-        editor.putString(KEY_TOKEN, token);
-        editor.putString(KEY_TOKEN_SECRET, tokenSecret);
-        editor.commit();
-        
-        Log.i(TAG, String.format("SAVED TOKEN: %s, %s, %s, %s", userName,
-                userId, token, tokenSecret));
-    }
-    
-    private void saveOAuth(OAuth oauth)
-    {
-        if (oauth == null)
-        {
-            Log.w(TAG, "Authorization failed");
-            return;
-        }
-        
-        User user = oauth.getUser();
-        OAuthToken oauthToken = oauth.getToken();
-        
-        if (user == null || oauthToken == null)
-        {
-            Log.w(TAG, "Authorization failed");
-            return;
-        }
-        
-        String userName = user.getUsername();
-        String userId = user.getId();
-        String token = oauthToken.getOauthToken();
-        String tokenSecret = oauthToken.getOauthTokenSecret();
-        
-        if (userName == null || userId == null || token == null ||
-                tokenSecret == null)
-        {
-            Log.w(TAG, "Authorization failed");
-            return;
-        }
-        
-        saveOAuth(userName, userId, token, tokenSecret);
-    }
-    
-    private OAuth loadOAuth()
-    {
-        SharedPreferences prefs = getSharedPreferences(OAUTH_PREFS,
-                Context.MODE_PRIVATE);
-        
-        String userName = prefs.getString(KEY_USER_NAME, null);
-        String userId = prefs.getString(KEY_USER_ID, null);
-        String token = prefs.getString(KEY_TOKEN, null);
-        String tokenSecret = prefs.getString(KEY_TOKEN_SECRET, null);
-        
-        OAuth result = new OAuth();
-        
-        if (userId != null)
-        {
-            User user = new User();
-            user.setUsername(userName);
-            user.setId(userId);
-            result.setUser(user);
-        }
-        
-        if (tokenSecret != null)
-            result.setToken(new OAuthToken(token, tokenSecret));
-        
-        return result;
     }
     
     private void redirectUserTo(String url)
@@ -258,26 +110,7 @@ public class MainActivity extends Activity
         String token = data[0].substring(data[0].indexOf("=") + 1);
         String verifier = data[1].substring(data[1].indexOf("=") + 1);
         
-        OAuth oauth = loadOAuth();
-        OAuthToken oauthToken = oauth.getToken();
-                
-        if (oauthToken == null)
-        {
-            Log.w(TAG, "Got a callback query, but no OAuth token secret is " +
-                    "defined yet");
-            return;
-        }
-        
-        String tokenSecret = oauthToken.getOauthTokenSecret();
-        
-        if (tokenSecret == null)
-        {
-            Log.e(TAG, "Got a callback query and found a OAuth token without " +
-                    "a token secret: looks like a bug");
-            return;
-        }
-        
-        new OauthAccessTokenRequestTask().execute(tokenSecret, token, verifier);
+        new OAuthFinishAuthenticationTask().execute(token, verifier);
     }
     
     private Flickr getFlickr()
