ceea52cc80fca6c0e393cd4463a28bf35e3c302a
Romulo A. Ceccon <romuloceccon@gmail.com>
2014-07-18 09:31:05 -0300

    Fixed retrieving of photo info and generation of html code

--- src/com/romuloceccon/hilgen/PhotosetActivity.java
+++ src/com/romuloceccon/hilgen/PhotosetActivity.java
@@ -2,15 +2,15 @@ package com.romuloceccon.hilgen;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import org.json.JSONException;
 
+import com.googlecode.flickrjandroid.Flickr;
 import com.googlecode.flickrjandroid.FlickrException;
 import com.googlecode.flickrjandroid.photos.Photo;
 import com.googlecode.flickrjandroid.photos.PhotoList;
-import com.googlecode.flickrjandroid.photos.PhotoUrl;
+import com.googlecode.flickrjandroid.photos.PhotosInterface;
 import com.googlecode.flickrjandroid.photos.Size;
 import com.googlecode.flickrjandroid.photosets.Photoset;
 import com.googlecode.flickrjandroid.photosets.PhotosetsInterface;
@@ -43,7 +43,10 @@ public class PhotosetActivity extends Activity
         protected List<Photo> doInBackground(Void... params)
         {
             List<Photo> result = new ArrayList<Photo>();
-            PhotosetsInterface intf = FlickrHelper.getFlickr().getPhotosetsInterface();
+            
+            Flickr f = FlickrHelper.getFlickr();
+            PhotosetsInterface photosetsIntf = f.getPhotosetsInterface();
+            PhotosInterface photosIntf = f.getPhotosInterface();
             
             PhotoList photos;
             int page = 1;
@@ -55,7 +58,7 @@ public class PhotosetActivity extends Activity
                 {
                     try
                     {
-                        photos = intf.getPhotos(photoset.getId(), perPage, page).getPhotoList();
+                        photos = photosetsIntf.getPhotos(photoset.getId(), perPage, page).getPhotoList();
                     }
                     catch (FlickrException e)
                     {
@@ -64,8 +67,14 @@ public class PhotosetActivity extends Activity
                         throw e;
                     }
                     
-                    for (Photo p: photos)
+                    for (Photo t: photos)
+                    {
+                        Log.d(TAG, "Getting photo " + t.getId());
+                        Photo p = photosIntf.getPhoto(t.getId());
+                        p.setSizes(photosIntf.getSizes(t.getId()));
                         result.add(p);
+                    }
+                    
                     page += 1;
                 } while (photos.size() >= perPage);
             }
@@ -140,18 +149,19 @@ public class PhotosetActivity extends Activity
         {
             Size size = p.getMediumSize();
             
+            int width = 0;
+            int height = 0;
+            String src = p.getMediumUrl();
+            
             if (size != null)
-                builder.append(getString(R.string.template_html_img,
-                        getPhotoPageUrl(p),
-                        p.getTitle(),
-                        p.getMediumUrl(),
-                        size.getWidth(),
-                        size.getHeight()));
-            else
-                builder.append(getString(R.string.template_html_img_without_size,
-                        getPhotoPageUrl(p),
-                        p.getTitle(),
-                        p.getMediumUrl()));
+            {
+                width = size.getWidth();
+                height = size.getHeight();
+                src = size.getSource();
+            }
+            
+            builder.append(getString(R.string.template_html_img,
+                    p.getUrl(), p.getTitle(), src, width, height));
         }
         
         String text = builder.toString();
@@ -159,33 +169,11 @@ public class PhotosetActivity extends Activity
         textPhotos.setText(text);
         
         ClipboardManager clipboard =
-                (ClipboardManager) getSystemService(CLIPBOARD_SERVICE); 
+                (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
         clipboard.setPrimaryClip(ClipData.newPlainText(
                 getString(R.string.label_clip_data_photo_list), text));
         
         showToast(getString(R.string.clipboard_text_set));
-    }
-    
-    private String getPhotoPageUrl(Photo p)
-    {
-        Collection<PhotoUrl> urls = p.getUrls();
-        
-        if (p.getUrl() == null)
-            Log.w(TAG, "NO URL!");
-        
-        if (urls == null)
-        {
-            Log.w(TAG, "NO PHOTO URLS!");
-            return null;
-        }
-        
-        for (PhotoUrl u: urls)
-        {
-            if (u.getType().compareTo("photopage") == 0)
-                return u.getUrl();
-        }
-        
-        return null;
     }
     
     private void showToast(CharSequence msg)
